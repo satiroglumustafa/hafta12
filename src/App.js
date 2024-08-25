1bb3f841f90ae6f821aa88c62a0d42ca8fb4e0e0
satiroglumustafa <mmustafa.satiroglu@gmail.com>
2024-08-25 17:57:28 +0300

    UseRef Örneği yapıldı

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const inputRef = useRef(null)
+  
+ 
+  useEffect(()=> {
+    inputRef.current.focus()
+  },[])
+
   return (
    <>
-    
+    <input ref={inputRef} type='text' placeholder='Adınız'/>
    </>
   );
 }
