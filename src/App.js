94a089e90bcad98119fe04e3495f5ad7518a2844
satiroglumustafa <mmustafa.satiroglu@gmail.com>
2024-08-25 18:53:05 +0300

    UseEffect 2 örneği

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+   
+  const [inputValue, setInputValue] = useState("");
+  const previousInputValue = useRef("");
+
+  useEffect(() => {
+    previousInputValue.current = inputValue;
+  }, [inputValue]);
+
   return (
-   <>
-    
-   </>
+    <>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <h2>Current Value: {inputValue}</h2>
+      <h2>Previous Value: {previousInputValue.current}</h2>
+    </>
   );
 }
 
